b2219173fede2f38a9694909eb8b8b06b1d465fa
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-02-20 08:41:55 -0300

    a funcao de atualizar dados do cliente esta pronta

--- src/main/java/Api/Rest/api/rest/Cliente/Cliente.java
+++ src/main/java/Api/Rest/api/rest/Cliente/Cliente.java
@@ -31,5 +31,19 @@ public class Cliente {
         this.email = dados.email();
         this.endereco = new Endereco(dados.endereco());
     }
+    public void atualizarInformacoes(DadosAtualizacaoCliente dados){
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.email() != null){
+            this.email = dados.email();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 
 }
--- src/main/java/Api/Rest/api/rest/Controller/ClienteController.java
+++ src/main/java/Api/Rest/api/rest/Controller/ClienteController.java
@@ -3,14 +3,12 @@ package Api.Rest.api.rest.Controller;
 
 import Api.Rest.api.rest.Cliente.Cliente;
 import Api.Rest.api.rest.Cliente.ClienteRepository;
+import Api.Rest.api.rest.Cliente.DadosAtualizacaoCliente;
 import Api.Rest.api.rest.Cliente.DadosCadastroCliente;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("clientes")
@@ -21,5 +19,11 @@ public class ClienteController {
     @Transactional
     public void cadastrarCliente(@RequestBody @Valid DadosCadastroCliente dados){
         clienteRepository.save(new Cliente(dados));
+    }
+    @PutMapping
+    @Transactional
+    public  void atualizarCliente(@RequestBody @Valid DadosAtualizacaoCliente dados){
+        var cliente = clienteRepository.getReferenceById(dados.id());
+        cliente.atualizarInformacoes(dados);
     }
 }
