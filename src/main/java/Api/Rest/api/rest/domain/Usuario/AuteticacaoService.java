fc39005e70ff7784faf055b48c9cf95c0c9af9d6
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-03-04 19:29:17 -0300

    feito um sistema de autenticacao para login de usuarios

---
+++ src/main/java/Api/Rest/api/rest/domain/Usuario/AuteticacaoService.java
@@ -0,0 +1,18 @@
+package Api.Rest.api.rest.domain.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuteticacaoService  implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return repository.findByLogin(login);
+    }
+}
---
+++ src/main/java/Api/Rest/api/rest/domain/Usuario/DadosAuteticacao.java
@@ -0,0 +1,4 @@
+package Api.Rest.api.rest.domain.Usuario;
+
+public record DadosAuteticacao(String login, String senha) {
+}
